d22dd05ee694e3801ee49d85dfdcc04b378ef103
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-11-08 21:37:48 -0300

    Definição da classe Saudacao como entidade do banco

--- ola-mundo-app/ola-mundo/src/main/java/com/ola/mundo/model/Saudacao.java
+++ ola-mundo-app/ola-mundo/src/main/java/com/ola/mundo/model/Saudacao.java
@@ -1,7 +1,15 @@
 package com.ola.mundo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Saudacao {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
     private String mensagem;
 
