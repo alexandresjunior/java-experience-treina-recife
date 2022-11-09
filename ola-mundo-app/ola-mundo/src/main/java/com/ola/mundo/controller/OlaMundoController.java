5903de9a27c53d34ab4289baa4b93755a8c0fd6c
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-11-08 21:20:23 -0300

    Definição dos parâmetros da requisição

--- ola-mundo-app/ola-mundo/src/main/java/com/ola/mundo/controller/OlaMundoController.java
+++ ola-mundo-app/ola-mundo/src/main/java/com/ola/mundo/controller/OlaMundoController.java
@@ -1,16 +1,19 @@
 package com.ola.mundo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ola.mundo.model.Saudacao;
 
 @RestController
 public class OlaMundoController {
-    
+
     @GetMapping("/ola")
-    public Saudacao digaOla() {
-        return new Saudacao(0, "Olá, mundo!");
+    public Saudacao digaOla(
+            @RequestParam(name = "id", required = false, defaultValue = "0") int id,
+            @RequestParam(name = "mensagem", required = false, defaultValue = "null") String mensagem) {
+        return new Saudacao(id, mensagem);
     }
 
 }
