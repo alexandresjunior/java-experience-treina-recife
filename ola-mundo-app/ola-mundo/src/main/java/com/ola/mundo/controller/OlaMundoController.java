1dc4284a5941bdf930027dee25ce3ff232f0cef8
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-11-10 20:34:49 -0300

    Métodos PUT, GET e DELETE

--- ola-mundo-app/ola-mundo/src/main/java/com/ola/mundo/controller/OlaMundoController.java
+++ ola-mundo-app/ola-mundo/src/main/java/com/ola/mundo/controller/OlaMundoController.java
@@ -1,8 +1,13 @@
 package com.ola.mundo.controller;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +28,38 @@ public class OlaMundoController {
     @PostMapping("/saudacao")
     public Saudacao salvarSaudacao(@RequestBody Saudacao saudacao) {
         return saudacaoRepository.save(saudacao);
+    }
+
+    @GetMapping("/saudacao")
+    public Iterable<Saudacao> obterTodasSaudacoes() {
+        return saudacaoRepository.findAll();
+    }
+
+    @GetMapping("/saudacao/{id}")
+    public Saudacao obterSaudacaoPorId(@PathVariable String id) {
+        return saudacaoRepository.findById(Integer.valueOf(id)).get();
+    }
+
+    @DeleteMapping("/saudacao/{id}")
+    public String deletarSaudacaoPeloId(@PathVariable String id) {
+        saudacaoRepository.deleteById(Integer.valueOf(id));
+
+        return "Registo deletado com sucesso!";
+    }
+
+    @PutMapping("/saudacao/{id}")
+    public Object atualizarSaudacao(
+        @PathVariable String id,
+        @RequestBody Saudacao saudacaoBody) {
+            try {
+                Saudacao saudacao = saudacaoRepository.findById(Integer.valueOf(id)).get();
+                
+                saudacao.setMensagem(saudacaoBody.getMensagem());
+
+                return saudacaoRepository.save(saudacao);
+            } catch (NoSuchElementException e) {
+                return "Não há saudação com id " + id;
+            }
     }
 
     @Autowired
