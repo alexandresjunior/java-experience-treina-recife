dcea662e4e7fb2d62aaed30ba79ad88c877fe4d4
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-11-10 11:26:45 -0300

    Substituição das tags anchor para Link

--- treina-recife-js/src/components/NavBar.js
+++ treina-recife-js/src/components/NavBar.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
     return (
         <aside className="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-light" id="sidenav-main">
             <div className="sidenav-header">
                 <i className="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-                <a className="navbar-brand m-0 text-center" href={"https://treinarecife.com.br/"}>
+                <Link className="navbar-brand m-0 text-center" to={`https://treinarecife.com.br/`}>
                     <img src={require("../assets/img/treina_recife_logo.png")} className="navbar-brand-img h-100" alt="treina_recife_logo" />
-                </a>
+                </Link>
             </div>
 
             <hr className="horizontal light mt-0 mb-2" />
@@ -13,72 +15,72 @@ const NavBar = () => {
             <div className="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link text-white active bg-gradient-warning" href="../pages/dashboard.html">
+                        <Link className="nav-link text-white active bg-gradient-warning" to={`/`}>
                             <div className="text-white text-center me-2 d-flex align-items-center justify-content-center">
                                 <i className="material-icons opacity-10">dashboard</i>
                             </div>
                             <span className="nav-link-text ms-1">Dashboard</span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-dark" href="../pages/tables.html">
+                        <Link className="nav-link text-dark" to={`/turmas`}>
                             <div className="text-dark text-center me-2 d-flex align-items-center justify-content-center">
                                 <i className="material-icons opacity-10">table_view</i>
                             </div>
                             <span className="nav-link-text ms-1">Turmas</span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-dark" href="../pages/billing.html">
+                        <Link className="nav-link text-dark" to={`/alunos`}>
                             <div className="text-dark text-center me-2 d-flex align-items-center justify-content-center">
                                 <i className="material-icons opacity-10">groups</i>
                             </div>
                             <span className="nav-link-text ms-1">Alunos</span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-dark" href="../pages/virtual-reality.html">
+                        <Link className="nav-link text-dark" to={`/cursos`}>
                             <div className="text-dark text-center me-2 d-flex align-items-center justify-content-center">
                                 <i className="material-icons opacity-10">devices</i>
                             </div>
                             <span className="nav-link-text ms-1">Cursos</span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-dark" href="../pages/rtl.html">
+                        <Link className="nav-link text-dark" to={`/professores`}>
                             <div className="text-dark text-center me-2 d-flex align-items-center justify-content-center">
                                 <i className="material-icons opacity-10">school</i>
                             </div>
                             <span className="nav-link-text ms-1">Professores</span>
-                        </a>
+                        </Link>
                     </li>
 
                     <li className="nav-item mt-3">
                         <h6 className="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-8">Dados do Usuário</h6>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-dark" href="../pages/profile.html">
+                        <Link className="nav-link text-dark" to={`/perfil`}>
                             <div className="text-dark text-center me-2 d-flex align-items-center justify-content-center">
                                 <i className="material-icons opacity-10">person</i>
                             </div>
                             <span className="nav-link-text ms-1">Perfil</span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-dark" href="../pages/sign-in.html">
+                        <Link className="nav-link text-dark" to={`/sign-in`}>
                             <div className="text-dark text-center me-2 d-flex align-items-center justify-content-center">
                                 <i className="material-icons opacity-10">login</i>
                             </div>
                             <span className="nav-link-text ms-1">Sign In</span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-dark" href="../pages/sign-up.html">
+                        <Link className="nav-link text-dark" to={`/sign-up`}>
                             <div className="text-dark text-center me-2 d-flex align-items-center justify-content-center">
                                 <i className="material-icons opacity-10">assignment</i>
                             </div>
                             <span className="nav-link-text ms-1">Sign Up</span>
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </div>
