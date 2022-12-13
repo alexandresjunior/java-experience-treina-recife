672000a0f9de89ef01f33076614f7632df69ee63
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-12-13 17:30:11 -0300

    Criação do projeto no Spring Boot

---
+++ treina-recife/src/test/java/com/treina/recife/TreinaRecifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treina.recife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinaRecifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
