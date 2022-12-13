1d7b99e97de58f71cf91c89f0fd4a5997d869aa8
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-12-13 17:43:56 -0300

    Criação da camada persistence de Prospect

--- treina-recife/src/main/java/com/treina/recife/model/Prospect.java
+++ treina-recife/src/main/java/com/treina/recife/model/Prospect.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 public class Prospect {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false)
