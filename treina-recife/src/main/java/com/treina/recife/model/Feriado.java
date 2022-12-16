f36623bb961aa2cfedc85214af30774ac47232cc
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-12-16 05:41:20 -0300

    Alteração para geração de ID com IDENTITY

--- treina-recife/src/main/java/com/treina/recife/model/Feriado.java
+++ treina-recife/src/main/java/com/treina/recife/model/Feriado.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 public class Feriado {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false)
